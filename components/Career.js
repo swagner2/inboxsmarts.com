26c73d86bca83deaae6254bab72c2c855243255f
Steven Wagner <steven@sales-ignition.com>
2023-08-18 17:44:42 -0400

    Update Career.js

--- components/Career.js
+++ components/Career.js
@@ -5,7 +5,7 @@ function Career() {
   Container.innerHTML = `
     <div class="row row-cols-1 row-cols-md-2 gy-5 align-items-center">
     <div class="col text-body">
-      <h2 class="title fw-bold">Join the Atlassian Talent Community</h2>
+      <h2 class="title fw-bold">Join the Inbox Smarts Team</h2>
       <p class="mt-2">
         Don’t see an exact role match? No problem! Join our Talent
         Community and stay up-to-date on company and careers updates
