4243538871662fd95d1d63f05cafcfd03d22e1fc
Steven Wagner <steven@sales-ignition.com>
2023-08-18 18:25:36 -0400

    Update Herosection.js

--- components/Herosection.js
+++ components/Herosection.js
@@ -17,8 +17,8 @@ function Herosection() {
               </p>
             </div>
             <div class="btns-group mt-3">
-              <a class="button btn-theme-default" href="#">Get Started</a>
-              <a class="button btn-theme-2" href="https://calendly.com/stevenwagner/discovery-call"
+              <a class="button btn-theme-default" href="https://calendly.com/stevenwagner/discovery-call">Get Started</a>
+              <a class="button btn-theme-2" href=""
                 ><i class="bi bi-play-circle me-2"></i> Watch Video</a>
             </div>
           </div>
